79a15b1a7f432b1ebf9eba5aa41a1abae2a95adb
Sijan14 <sisijan18@earlham.edu>
2024-12-18 23:07:17 -0500

    Added page for blogs

---
+++ probability_distribution/app.R
@@ -0,0 +1,57 @@
+#
+# This is a Shiny web application. You can run the application by clicking
+# the 'Run App' button above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+
+# Account set up
+install.packages("rsconnect")
+library(rsconnect)
+library(shiny)
+rsconnect::setAccountInfo(name='sijan1', token='9D617B9E01C6F1D0BDA280B3780536EC', secret='0k/8PTAn91Ye5TTGI/Q3WiZE9IvgsX+0bMoG2GEF')
+
+
+# Define UI for application that draws a histogram
+ui <- fluidPage(
+
+    # Application title
+    titlePanel("Old Faithful Geyser Data"),
+
+    # Sidebar with a slider input for number of bins 
+    sidebarLayout(
+        sidebarPanel(
+            sliderInput("bins",
+                        "Number of bins:",
+                        min = 1,
+                        max = 50,
+                        value = 30)
+        ),
+
+        # Show a plot of the generated distribution
+        mainPanel(
+           plotOutput("distPlot")
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+
+    output$distPlot <- renderPlot({
+        # generate bins based on input$bins from ui.R
+        x    <- faithful[, 2]
+        bins <- seq(min(x), max(x), length.out = input$bins + 1)
+
+        # draw the histogram with the specified number of bins
+        hist(x, breaks = bins, col = 'darkgray', border = 'white',
+             xlab = 'Waiting time to next eruption (in mins)',
+             main = 'Histogram of waiting times')
+    })
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
+
+rsconnect::deployApp('/Users/sijanikbal/Documents/Ph.D. in I:O Psychology/*Fall 2024/Sijan_Portfolio/probability_distribution')
